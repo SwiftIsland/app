620823de9e28fbab2c61f9810019c7496b1ccf81
Niels van Hoorn <nvh@nvh.io>
2023-07-12 12:20:51 +0200

    Add basic NFC tag reading support

--- SwiftIslandApp/MainApp.swift
+++ SwiftIslandApp/MainApp.swift
@@ -15,6 +15,14 @@ struct MainApp: App {
     var body: some Scene {
         WindowGroup {
             TabBarView()
+                .onContinueUserActivity(NSUserActivityTypeBrowsingWeb) { activity in
+                    if let URL = activity.webpageURL,
+                       URL.host == "swiftisland.nl",
+                       let components = URLComponents(url: URL, resolvingAgainstBaseURL: true),
+                       let nfc = components.queryItems?.first(where: {$0.name == "nfc"})?.value {
+                        print("NFC: \(nfc)")
+                    }
+            }
         }
     }
 }
