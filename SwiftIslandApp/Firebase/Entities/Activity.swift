a4712f3b6b844e40d9b88f229b83d0acb2520a0c
Paul Peelen <Paul@PaulPeelen.com>
2023-07-25 10:52:17 +0200

    Added activity

---
+++ SwiftIslandApp/Firebase/Entities/Activity.swift
@@ -0,0 +1,20 @@
+//
+// Created by Paul Peelen for the use in the Swift Island app
+// Copyright © 2023 AppTrix AB. All rights reserved.
+//
+
+import Foundation
+
+struct Activity: Response {
+    let id: String
+    let title: String
+    let description: String
+    let mentors: [String]
+    let type: String
+    let imageName: String?
+    let duration: TimeInterval
+}
+
+extension Activity: Identifiable { }
+
+extension Activity: Hashable { }
