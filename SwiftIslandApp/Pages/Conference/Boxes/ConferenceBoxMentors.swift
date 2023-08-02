cae78cb1bee24810c84034f0738efe10c53e69e0
Paul Peelen <Paul@PaulPeelen.com>
2023-08-02 23:55:57 +0200

    Code refactoring and added support for different home pages depending on user state

---
+++ SwiftIslandApp/Pages/Conference/Boxes/ConferenceBoxMentors.swift
@@ -0,0 +1,65 @@
+//
+// Created by Paul Peelen for the use in the Swift Island app
+// Copyright © 2023 AppTrix AB. All rights reserved.
+//
+
+import SwiftUI
+import SwiftIslandDataLogic
+
+struct ConferenceBoxMentors: View {
+    var namespace: Namespace.ID
+
+    let geo: GeometryProxy
+
+    @EnvironmentObject private var appDataModel: AppDataModel
+
+    @Binding var isShowingMentor: Bool
+    @Binding var mayShowMentorNextMentor: Bool
+    @Binding var selectedMentor: Mentor?
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Mentors this year".uppercased())
+                .font(.caption)
+                .foregroundColor(.secondary)
+                .padding(.horizontal, 40)
+                .padding(.top, 6)
+                .padding(.bottom, 0)
+            TabView {
+                ForEach(appDataModel.mentors) { mentor in
+                    MentorView(namespace: namespace, mentor: mentor, isShowContent: $isShowingMentor)
+                        .matchedGeometryEffect(id: mentor.id, in: namespace)
+                        .mask {
+                            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                        }
+                        .padding(.horizontal, 20)
+                        .onTapGesture {
+                            if mayShowMentorNextMentor {
+                                mayShowMentorNextMentor = false
+                                selectedMentor = mentor
+                                withAnimation(.interactiveSpring(response: 0.55, dampingFraction: 0.8)) {
+                                    isShowingMentor = true
+                                }
+                            } else {
+                                debugPrint("Too soon to show next mentor animation")
+                            }
+                        }
+                }
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .frame(minHeight: geo.size.width * 0.80)
+        }
+    }
+}
+
+struct ConferenceBoxMentors_Previews: PreviewProvider {
+    @Namespace static var namespace
+
+    static var previews: some View {
+        let appDataModel = AppDataModel()
+        GeometryReader { geo in
+            ConferenceBoxMentors(namespace: namespace, geo: geo, isShowingMentor: .constant(false), mayShowMentorNextMentor: .constant(true), selectedMentor: .constant(nil))
+                .environmentObject(appDataModel)
+        }
+    }
+}
--- SwiftIslandApp/Pages/Conference/ConferencePageContentView.swift
+++ SwiftIslandApp/Pages/Conference/ConferencePageContentView.swift
@@ -23,46 +23,34 @@ struct ConferencePageContentView: View {
     var body: some View {
         GeometryReader { geo in
             ScrollView(.vertical) {
-                ConferenceHeaderView()
-                ConferenceBoxTicket()
-                    .padding(.vertical, 6)
+                if !userIsActivated {
+                    ConferenceHeaderView()
+                    ConferenceBoxTicket()
+                        .padding(.vertical, 6)
 
-                VStack(alignment: .leading) {
-                    Text("Mentors this year".uppercased())
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                        .padding(.horizontal, 40)
-                        .padding(.top, 6)
-                        .padding(.bottom, 0)
-                    TabView {
-                        ForEach(appDataModel.mentors) { mentor in
-                            MentorView(namespace: namespace, mentor: mentor, isShowContent: $isShowingMentor)
-                                .matchedGeometryEffect(id: mentor.id, in: namespace)
-                                .mask {
-                                    RoundedRectangle(cornerRadius: 15, style: .continuous)
-                                }
-                                .padding(.horizontal, 20)
-                                .onTapGesture {
-                                    if mayShowMentorNextMentor {
-                                        mayShowMentorNextMentor = false
-                                        selectedMentor = mentor
-                                        withAnimation(.interactiveSpring(response: 0.55, dampingFraction: 0.8)) {
-                                            isShowingMentor = true
-                                        }
-                                    } else {
-                                        debugPrint("Too soon to show next mentor animation")
-                                    }
-                                }
-                        }
+                    ConferenceBoxMentors(namespace: namespace,
+                                         geo: geo,
+                                         isShowingMentor: $isShowingMentor,
+                                         mayShowMentorNextMentor: $mayShowMentorNextMentor,
+                                         selectedMentor: $selectedMentor)
+
+                    if let nextEvent {
+                        ConferenceBoxEvent(event: nextEvent)
+                    } else {
+                        ProgressView()
                     }
-                    .tabViewStyle(.page(indexDisplayMode: .never))
-                    .frame(minHeight: geo.size.width * 0.80)
-                }
-
-                if let nextEvent {
-                    ConferenceBoxEvent(event: nextEvent)
                 } else {
-                    ProgressView()
+                    if let nextEvent {
+                        ConferenceBoxEvent(event: nextEvent)
+                    } else {
+                        ProgressView()
+                    }
+
+                    ConferenceBoxMentors(namespace: namespace,
+                                         geo: geo,
+                                         isShowingMentor: $isShowingMentor,
+                                         mayShowMentorNextMentor: $mayShowMentorNextMentor,
+                                         selectedMentor: $selectedMentor)
                 }
 
                 // Removed for now.
@@ -78,3 +66,4 @@ struct ConferencePageContentView: View {
         }
     }
 }
+
