1668fda7113eb066d99c609aab51ceffefb5f743
Niels van Hoorn <nvh@users.noreply.github.com>
2023-08-04 07:07:48 +0200

    Hide mentor scrollview indicators

--- SwiftIslandApp/Pages/Conference/ConferencePageContentView.swift
+++ SwiftIslandApp/Pages/Conference/ConferencePageContentView.swift
@@ -22,7 +22,7 @@ struct ConferencePageContentView: View {
 
     var body: some View {
         GeometryReader { geo in
-            ScrollView(.vertical) {
+            ScrollView(.vertical, showsIndicators: false) {
                 if !userIsActivated {
                     ConferenceHeaderView()
                     ConferenceBoxTicket()
