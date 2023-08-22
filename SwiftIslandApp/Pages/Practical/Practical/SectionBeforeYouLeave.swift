53374376062d23417f11dc7fd8f3ca57e5b328c0
Niels van Hoorn <nvh@nvh.io>
2023-08-22 11:38:43 +0200

    Always show tickets row

--- SwiftIslandApp/Pages/Practical/Practical/SectionBeforeYouLeave.swift
+++ SwiftIslandApp/Pages/Practical/Practical/SectionBeforeYouLeave.swift
@@ -16,15 +16,13 @@ struct SectionBeforeYouLeave: View {
                 Text("Make sure you are all set for a few days of awesome workshops.")
                     .font(.subheadline)
             }
-            if appDataModel.tickets.count > 0 {
-                NavigationLink(value: NavigationPage.tickets) {
-                    HStack {
-                        Image(systemName: "ticket")
-                            .foregroundColor(.questionMarkColor)
-                            .frame(maxWidth: iconMaxWidth)
-                        Text("Tickets")
-                            .dynamicTypeSize(.small ... .medium)
-                    }
+            NavigationLink(value: NavigationPage.tickets) {
+                HStack {
+                    Image(systemName: "ticket")
+                        .foregroundColor(.questionMarkColor)
+                        .frame(maxWidth: iconMaxWidth)
+                    Text("Tickets")
+                        .dynamicTypeSize(.small ... .medium)
                 }
             }
             NavigationLink(value: NavigationPage.packlist) {
