b8a8bd491987fe058b894817039e952133a1faf2
Niels van Hoorn <nvh@nvh.io>
2023-08-30 02:05:14 +0200

    Dark mode support

--- SwiftIslandApp/Pages/Practical/Spot the Seal/PuzzleItemView.swift
+++ SwiftIslandApp/Pages/Practical/Spot the Seal/PuzzleItemView.swift
@@ -11,24 +11,34 @@ let fromAngle: Double = -45 - sealAngle
 let toAngle: Double = 45 - sealAngle
 
 let puzzleItemSize: CGFloat = 88
-struct PuzzleItemView: View {
-    @State var angle = Angle(degrees: Double.random(in: fromAngle..<toAngle)) 
-    @State var x = CGFloat.random(in: -5...4)
-    @State var y = CGFloat.random(in: -5...4)
-    @State var puzzle: Puzzle
-    @State var scale: Double = 1
-    @State var flipAngle: CGFloat
-    @State var isCurrent: Bool = false
-    
+class PuzzleItemViewModel: ObservableObject {
+    @Published var angle = Angle(degrees: Double.random(in: fromAngle..<toAngle))
+    @Published var x = CGFloat.random(in: -5...4)
+    @Published var y = CGFloat.random(in: -5...4)
+    @Published var puzzle: Puzzle
+    @Published var scale: Double = 1
+    @Published var flipAngle: CGFloat = 0
+    @Published var isCurrent: Bool = false
+
     init(puzzle: Puzzle, isCurrent: Bool = false) {
         self.puzzle = puzzle
         self.isCurrent = isCurrent
         self.flipAngle = puzzle.state == .Solved ? 180 : 0
     }
+}
+
+struct PuzzleItemView: View {
+    @StateObject var viewModel: PuzzleItemViewModel
+    @Environment(\.colorScheme) var colorScheme
+
+    init(puzzle: Puzzle, isCurrent: Bool = false) {
+        _viewModel = StateObject(wrappedValue: PuzzleItemViewModel(puzzle: puzzle, isCurrent: isCurrent))
+    }
 
     var body: some View {
+        let color: Color = colorScheme == .light ? .black : .white
+        let puzzle = viewModel.puzzle
         let stack = ZStack {
-            
             Text("\(puzzle.number)")
                 .font(.custom("WorkSans-Bold", size: 30))
             Rectangle().stroke(style: StrokeStyle(lineWidth: 2))  
@@ -37,26 +47,27 @@ struct PuzzleItemView: View {
                 .resizable(capInsets: EdgeInsets(), resizingMode:.stretch)
                 .aspectRatio(1, contentMode: .fit)
                 .foregroundColor(puzzle.color)
-                .rotationEffect(angle, anchor: .center)
+                .rotationEffect(viewModel.angle, anchor: .center)
                 .rotation3DEffect(
-                    .degrees(flipAngle),
+                    .degrees(viewModel.flipAngle),
                                           axis: (x: 0.0, y: 1.0, z: 0.0),
                                           anchor: .center,
                                           anchorZ: 0.0,
                                           perspective: 1.0
                 )
-                .offset(x: x, y: y)
-                .scaleEffect(scale)
-        }.aspectRatio(1, contentMode: .fit)
+                .offset(x: viewModel.x, y: viewModel.y)
+                .scaleEffect(viewModel.scale)
+        }
+            .aspectRatio(1, contentMode: .fit)
+            .foregroundColor(color)
         switch (puzzle.state) {
         case .Found:
             stack
                 .onAppear {
-                    print("onAppear \(isCurrent)")
-                    if (isCurrent) {
-                        scale = 20
+                    if (viewModel.isCurrent) {
+                        viewModel.scale = 20
                         withAnimation(.easeInOut(duration: 0.3)) {
-                            scale = 1
+                            viewModel.scale = 1
                         }
                     }
                 }
