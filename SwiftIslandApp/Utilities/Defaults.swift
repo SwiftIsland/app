082895a67c7796ad9a13351c752237363f5100bf
Paul Peelen <Paul@PaulPeelen.com>
2024-05-29 21:42:30 +0200

    Made sure to reset the packligt from previous year

--- SwiftIslandApp/Utilities/Defaults.swift
+++ SwiftIslandApp/Utilities/Defaults.swift
@@ -7,6 +7,7 @@ import Defaults
 import SwiftIslandDataLogic
 
 extension Defaults.Keys {
+    static let isPacklistSetupFor2024 = Key<Bool>("isPacklistSetupFor2024", default: false)
     static let userIsActivated = Key<Bool>("userActivated2024", default: false)
     static let packingItems = Key<[PackingItem]>("packingList", default: [])
     static let hasShownTicketReminder = Key<Bool>("hasShownTicketReminder", default: false)
