1e8027c0a51fd0b270425cbb70e93cd8e62840ab
Paul Peelen <Paul@PaulPeelen.com>
2024-05-29 21:35:24 +0200

    Deactivating users and also removing mentors for activated users

--- SwiftIslandApp/Utilities/Defaults.swift
+++ SwiftIslandApp/Utilities/Defaults.swift
@@ -7,7 +7,7 @@ import Defaults
 import SwiftIslandDataLogic
 
 extension Defaults.Keys {
-    static let userIsActivated = Key<Bool>("userActivated", default: false)
+    static let userIsActivated = Key<Bool>("userActivated2024", default: false)
     static let packingItems = Key<[PackingItem]>("packingList", default: [])
     static let hasShownTicketReminder = Key<Bool>("hasShownTicketReminder", default: false)
 }
