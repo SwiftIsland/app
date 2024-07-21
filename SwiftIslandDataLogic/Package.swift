10c85f000154482a1711a836733cb2a00c56909d
Paul Peelen <Paul@PaulPeelen.com>
2024-07-21 21:12:11 +0200

    Bumping min version to iOS 17

--- SwiftIslandDataLogic/Package.swift
+++ SwiftIslandDataLogic/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.7.1
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftIslandDataLogic",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v17)],
     products: [
         .library(
             name: "SwiftIslandDataLogic",
