df02367853ec8c3007af243f2ddc40708cb83b1b
Paul Peelen <Paul@PaulPeelen.com>
2024-08-18 21:24:18 +0200

    Workshop steps

--- SwiftIslandApp/Pages/Common/TabBarView.swift
+++ SwiftIslandApp/Pages/Common/TabBarView.swift
@@ -7,63 +7,198 @@ import SwiftUI
 import Defaults
 import SwiftIslandDataLogic
 
-private enum Tab: CaseIterable {
+// 1.1: Rename to "Tabs" instead of "Tab"
+private enum Tabs {
     case home
     case practical
     case schedule
+    case mentorList
+    case search
+    case mentors(Mentor)
+
+    var customizationID: String {
+        switch self {
+        case .mentors(let mentor):
+            mentor.customizationID
+        default:
+            "\(self.hashValue)"
+        }
+    }
+}
+
+extension Mentor {
+    var customizationID: String {
+        "SwiftIsland.mentor." + self.name
+    }
+}
+
+extension Tabs: Hashable {
+    var id: String {
+        switch self {
+        case .home: "home"
+        case .practical: "practical"
+        case .schedule: "schedule"
+        case .mentorList: "mentorList"
+        case .mentors(let mentor): mentor.id
+        case .search: "search"
+        }
+    }
 }
 
 struct TabBarView: View {
     @Namespace private var namespace
-    @State private var selectedItem: Tab = .home
+    @State private var selectedItem: Tabs = .home
     @Binding var appActionTriggered: AppActions?
 
     @State private var isShowingMentor = false
     @EnvironmentObject private var appDataModel: AppDataModel
 
+    @AppStorage("sidebarCustomizations") var tabViewCustomization: TabViewCustomization
+
+    @State private var favoriteMentors: [Mentor] = []
+
+    @State var mentorPath: NavigationPath = .init()
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+
     @Default(.userIsActivated)
     private var userIsActivated
 
     var body: some View {
-        let tabs: [Tab] = {
-            if userIsActivated {
-                return [.home, .practical, .schedule]
-            } else {
-                return [.home, .practical]
-            }
-        }()
-
         ZStack {
+            // 1.2. Update syntax to use Tab in TabView
             TabView(selection: $selectedItem) {
-                ForEach(tabs, id: \.self) { tab in
-                    switch tab {
-                    case .home:
+                Tab("Conference", systemImage: "person.3", value: .home) {
+                    NavigationStack {
                         ConferencePageView(namespace: namespace, isShowingMentor: $isShowingMentor)
-                            .tabItem {
-                                Label("Conference", systemImage: "person.3")
-                                    .environment(\.symbolVariants, tab == selectedItem ? .fill : .none)
+                            .navigationDestination(for: Mentor.self) { mentor in
+                                MentorPageView(mentor: mentor)
+                                    .navigationTransition(.zoom(sourceID: mentor.id, in: namespace))
                             }
-                            .tag(tab)
-                    case .practical:
-                        PracticalPageView()
-                            .tabItem {
-                                Label("Practical", systemImage: "wallet.pass")
-                                    .environment(\.symbolVariants, tab == selectedItem ? .fill : .none)
-                            }
-                            .tag(tab)
-                    case .schedule:
-                        NavigationStack {
-                            ScheduleView()
-                        }
-                        .tabItem {
-                            Label("Schedule", systemImage: "calendar.day.timeline.left")
-                                .environment(\.symbolVariants, tab == selectedItem ? .fill : .none)
-                        }
-                        .tag(tab)
                     }
                 }
+                .customizationBehavior(.disabled, for: .sidebar, .tabBar)
+
+                Tab("Practical", systemImage: "wallet.pass", value: .practical) {
+                    PracticalPageView()
+                }
+                .customizationID(Tabs.practical.customizationID)
+
+                Tab("Schedule", systemImage: "calendar.day.timeline.left", value: .schedule) {
+                    NavigationStack {
+                        ScheduleView()
+                    }
+                }
+                .customizationID(Tabs.schedule.customizationID)
+                .badge(2)
+
+                Tab(value: .search, role: .search) {
+                    MentorListView()
+                }
+
+                // 2.0 Add Mentor tab
+                //                if UIDevice.current.userInterfaceIdiom != .phone {
+                //                    Tab("Mentors", systemImage: "graduationcap", value: .mentors) {
+                //                        NavigationStack(path: $mentorPath) {
+                //                            MentorListView()
+                //                                .environmentObject(appDataModel)
+                //                                .navigationDestination(for: Mentor.self) { mentor in
+                //                                    MentorPageView(mentor: mentor)
+                //                                }
+                //                        }
+                //                    }
+                //                }
+
+                // 2.1 Add mentor section
+
+            TabSection {
+                ForEach(appDataModel.mentors) { mentor in
+                    Tab(mentor.name, systemImage: "graduationcap", value: Tabs.mentors(mentor)) {
+                        NavigationStack(path: $mentorPath) {
+                            MentorPageView(mentor: mentor)
+                        }
+                    }
+                    .customizationID(mentor.customizationID)
+
+                }
+            } header: {
+                Label("Mentors", systemImage: "graduationcap")
             }
-            .accentColor(.questionMarkColor)
+            .defaultVisibility(.hidden, for: .tabBar)
+            .customizationID("mentorList")
+            .sectionActions {
+                Button {
+                    tabViewCustomization.resetSectionOrder()
+                } label: {
+                    Label("Reset Section Order", systemImage: "arrow.counterclockwise")
+                }
+
+                Button {
+                    tabViewCustomization.resetVisibility()
+                } label: {
+                    Label("Reset Visibility", systemImage: "arrow.counterclockwise")
+                }
+            }
+            .hidden(horizontalSizeClass == .compact)
+
+                TabSection {
+                    ForEach(favoriteMentors) { mentor in
+                        Tab(mentor.name, systemImage: "graduationcap", value: Tabs.mentors(mentor)) {
+                            NavigationStack(path: $mentorPath) {
+                                MentorPageView(mentor: mentor)
+                            }
+                        }
+                        .customizationID("favMentor.\(mentor.customizationID)")
+                        .defaultVisibility(.hidden, for: .tabBar)
+                    }
+
+                    Tab("Mentor list", systemImage: "graduationcap", value: .mentorList) {
+                        NavigationStack {
+                            MentorListView()
+                        }
+                    }
+                    .customizationID(Tabs.mentorList.customizationID)
+                    .customizationBehavior(.disabled, for: .sidebar, .tabBar)
+                    .defaultVisibility(.hidden, for: .tabBar)
+                } header: {
+                    Label("Favorite mentors", systemImage: "heart")
+                }
+                .defaultVisibility(.hidden, for: .tabBar)
+                .customizationID("favoriteMentorList")
+                .dropDestination(for: Mentor.self) { mentors in
+                    favoriteMentors.append(contentsOf: mentors)
+                }
+                .hidden(horizontalSizeClass == .compact)
+            }
+            // 1.3 Enable sidebar¶
+            .tabViewStyle(.sidebarAdaptable)
+            .tabViewCustomization($tabViewCustomization)
+            .tabViewSidebarHeader {
+                Image("Logo")
+                VStack(spacing: 0) {
+                    Text("Swift")
+                        .font(.custom("WorkSans-Bold", size: 64))
+                        .foregroundColor(.logoText)
+                    Text("Island")
+                        .font(.custom("WorkSans-Regular", size: 60))
+                        .foregroundColor(.logoText)
+                        .offset(CGSize(width: 0, height: -20))
+                }
+            }
+            .tabViewSidebarFooter {
+                VStack(spacing: 8) {
+                    Button {
+                        tabViewCustomization.resetVisibility()
+                        tabViewCustomization.resetSectionOrder()
+                    } label: {
+                        Text("Reset sidebar")
+                            .font(.footnote)
+                    }
+
+                    Text("App version: \(Bundle.main.appVersionLong) (\(Bundle.main.appBuild))")
+                        .font(.footnote)
+                        .foregroundStyle(.secondary)
+                }
+            }
         }
         .onAppear {
             handleAppAction()
@@ -88,6 +223,12 @@ struct TabBarView: View {
 
             self.appActionTriggered = nil
         }
+    }
+}
+
+extension Mentor: @retroactive Transferable {
+    public static var transferRepresentation: some TransferRepresentation {
+        CodableRepresentation(contentType: .content)
     }
 }
 
--- SwiftIslandApp/Pages/Mentor/MentorListView.swift
+++ SwiftIslandApp/Pages/Mentor/MentorListView.swift
@@ -15,60 +15,62 @@ struct MentorListView: View {
     var body: some View {
         VStack {
             List(appDataModel.mentors) { mentor in
-                HStack {
-                    Image(mentor.imageName)
-                        .resizable()
-                        .cornerRadius(15)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100, height: 100)
-//                        .frame(maxHeight: 100)
-                        .scaledToFill()
-                        .border(Color(.sRGB, red: 150 / 255, green: 150 / 255, blue: 150 / 255, opacity: 0.1), width: 1)
-                    VStack(alignment: .leading) {
-                        Text(mentor.name)
-                            .font(.title)
+                NavigationLink(value: mentor) {
+                    HStack {
+                        Image(mentor.imageName)
+                            .resizable()
+                            .cornerRadius(15)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100, height: 100)
+                            .scaledToFill()
+                            .border(Color(.sRGB, red: 150 / 255, green: 150 / 255, blue: 150 / 255, opacity: 0.1), width: 1)
+                        VStack(alignment: .leading) {
+                            Text(mentor.name)
+                                .font(.title)
+                                .foregroundColor(.primary)
+                            HStack(alignment: .center) {
+                                if let web = mentor.webUrl {
+                                    Button(action: {
+                                        showUrl = web
+                                    }, label: {
+                                        Image("web")
+                                    })
+                                    .buttonStyle(.plain)
+                                    .padding(0)
+                                    .foregroundColor(.red)
+                                }
+                                if let mastodon = mentor.mastodonUrl {
+                                    Button(action: {
+                                        showUrl = mastodon
+                                    }, label: {
+                                        Image("mastodon")
+                                    })
+                                    .buttonStyle(.plain)
+                                }
+                                if let linkedin = mentor.linkedInUrl {
+                                    Button(action: {
+                                        showUrl = linkedin
+                                    }, label: {
+                                        Image("linkedin")
+                                    })
+                                    .buttonStyle(.plain)
+                                }
+                                if let twitter = mentor.twitterUrl {
+                                    Button(action: {
+                                        showUrl = twitter
+                                    }, label: {
+                                        Image("x")
+                                    })
+                                    .buttonStyle(.plain)
+                                    .padding(0)
+                                }
+                                Spacer()
+                            }
                             .foregroundColor(.primary)
-                        HStack(alignment: .center) {
-                            if let web = mentor.webUrl {
-                                Button(action: {
-                                    showUrl = web
-                                }, label: {
-                                    Image("web")
-                                })
-                                .buttonStyle(.plain)
-                                .padding(0)
-                                .foregroundColor(.red)
-                            }
-                            if let mastodon = mentor.mastodonUrl {
-                                Button(action: {
-                                    showUrl = mastodon
-                                }, label: {
-                                    Image("mastodon")
-                                })
-                                .buttonStyle(.plain)
-                            }
-                            if let linkedin = mentor.linkedInUrl {
-                                Button(action: {
-                                    showUrl = linkedin
-                                }, label: {
-                                    Image("linkedin")
-                                })
-                                .buttonStyle(.plain)
-                            }
-                            if let twitter = mentor.twitterUrl {
-                                Button(action: {
-                                    showUrl = twitter
-                                }, label: {
-                                    Image("x")
-                                })
-                                .buttonStyle(.plain)
-                                .padding(0)
-                            }
-                            Spacer()
                         }
-                        .foregroundColor(.primary)
                     }
                 }
+                .draggable(mentor)
             }
         }
         .task {
